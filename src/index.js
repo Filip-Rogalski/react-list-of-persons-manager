7d12a2fb654ec220133a45ac0099b90db02317ca
Filip Rogalski <f.rogalski@gmail.com>
2017-06-27 22:51:47 +0200

    modified readme, several new functionalities

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ class App extends Component {
         super();
         this.sendPersonToList = this.sendPersonToList.bind(this);
         this.deletePersonFromList = this.deletePersonFromList.bind(this);
-        this.editPerson = this.editPerson.bind(this);
+        this.updateEditedPerson = this.updateEditedPerson.bind(this);
         this.sortPersonsByAge = this.sortPersonsByAge.bind(this);
+        this.clearInputs = this.clearInputs.bind(this);
         this.sortPersonsByName = this.sortPersonsByName.bind(this);
+        this.editModeOn = this.editModeOn.bind(this);
+        this.editPerson = this.editPerson.bind(this);
         this.getName = this.getName.bind(this);
         this.getAge = this.getAge.bind(this);
         this.id = 0;
-        this.state = {persons: [], newPersonsName: '', newPersonsAge: ''};
+        this.state = {persons: [], newPersonsName: '', newPersonsAge: '', editMode: 'false', editedItemId: ''};
     }
     
     getName(e){
@@ -26,6 +29,19 @@ class App extends Component {
     
     sendPersonToList(e){
         e.preventDefault();
+        const nameInput = e.target.parentElement.firstChild,
+            ageInput = e.target.parentElement.firstChild.nextSibling;
+        this.setState({persons: this.state.persons.concat([[this.id, this.state.newPersonsName, this.state.newPersonsAge]])}, this.clearInputs(nameInput, ageInput));
+        this.id += 1;
+    }
+    
+    clearInputs(firstInput, secondInput){
+        firstInput.value = '';
+        secondInput.value = '';
+    }
+    
+    updateEditedPerson1(e){
+        e.preventDefault();
         this.setState({persons: this.state.persons.concat([[this.id, this.state.newPersonsName, this.state.newPersonsAge]])});
         this.id += 1;
     }
@@ -35,9 +51,19 @@ class App extends Component {
         this.setState(prevState => ({ persons: prevState.persons.filter(person => person[0] !== removedItemId) }));
     }
     
+    updateEditedPerson(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ persons: prevState.persons.map(person => person[0] === this.state.editedItemId ? [this.state.editedItemId, this.state.newPersonsName, this.state.newPersonsAge] : person), editMode: 'false' }));
+    }
+    
+    editModeOn(e) {
+        this.setState({editMode: 'true', editedItemId: parseInt(e.target.parentElement.dataset.id, 10)});
+    }
+    
     editPerson(e) {
-        console.log(e.target);
-        /* dokończyć obsługę edycji */
+        /* chwilowo zbędna funkcja */
+        this.setState({editedItemId: parseInt(e.target.parentElement.dataset.id, 10)});
+        console.log(e.target.parentElement.dataset.id);
     }
     
     sortPersonsByAge(e) {
@@ -52,8 +78,8 @@ class App extends Component {
         return (
             <div>
                 <h1>Manage person list</h1>
-                <AddPersonForm getAge={this.getAge} getName={this.getName} submitHandler={this.sendPersonToList}/>
-                <ResultList editItem={this.editPerson} deleteItem={this.deletePersonFromList} personsData={this.state.persons}/>
+                <AddPersonForm getAge={this.getAge} getName={this.getName} submitHandler={this.sendPersonToList} submitEditHandler={this.updateEditedPerson} editMode={this.state.editMode}/>
+                <ResultList editModeOn={this.editModeOn} editPerson={this.editPerson} deleteItem={this.deletePersonFromList} personsData={this.state.persons}/>
                 <button className="sortBtn" onClick={this.sortPersonsByAge}>Sortuj Wiek</button>
                 <button className="sortBtn" onClick={this.sortPersonsByName}>Sortuj Nazwisko</button>
             </div>
@@ -68,7 +94,7 @@ class AddPersonForm extends Component {
                 <form>
                     <input type="text" id="name" placeholder="Wpisz nazwisko" onChange={this.props.getName}/>
                     <input type="text" id="age" placeholder="Wpisz wiek" onChange={this.props.getAge}/>
-                    <input type="submit" value="Dodaj" onClick={this.props.submitHandler}/>
+            {this.props.editMode === 'false' ? (<button onClick={this.props.submitHandler}>Dodaj</button>) : (<button onClick={this.props.submitEditHandler}>Zatwierdź</button>)}
                 </form>
             </div>
         )
@@ -86,18 +112,32 @@ class DeleteButton extends Component {
 class EditButton extends Component {
     render(){
         return(
-            <button onClick={this.props.editItem} className="itemBtn editBtn">Edytuj</button>
+            <button onClick={this.props.editModeOn} className="itemBtn editBtn">Edytuj</button>
         )
     }
 }
 
 class ResultList extends Component {
+    constructor(){
+        super();
+        this.displayData = this.displayData.bind(this);
+        this.hideData = this.hideData.bind(this);
+    }
+    
+    displayData(e){
+        ReactDOM.render(<span> Age: {e.target.parentElement.dataset.age}</span>, e.target.nextSibling);
+    }
+
+    hideData(e) {
+        ReactDOM.render(<span></span>, e.target.nextSibling);
+    }
+    
     render(){
         return (
             <div className="row">
                 <ul>
                     {this.props.personsData.map(item => (
-                    <li key={item[0]} data-id={item[0]} data-age={item[2]} data-name={item[1]}><span>{item[1]}</span><DeleteButton deleteItem={this.props.deleteItem}/><EditButton editItem={this.props.editItem}/></li>
+                    <li key={item[0]} data-id={item[0]} data-age={item[2]} data-name={item[1]}><span onMouseEnter={this.displayData} onMouseLeave={this.hideData} onClick={this.props.editPerson}>{item[1]}</span><span></span><DeleteButton deleteItem={this.props.deleteItem}/><EditButton editModeOn={this.props.editModeOn}/></li>
                     ))}
                 </ul>
             </div>
